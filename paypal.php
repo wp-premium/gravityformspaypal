8e3e13d4040686c551309a002cdce0c5ca992f84
Gravity Forms <info@gravityforms.com>
2020-10-01 14:18:12 +0200

    3.4

--- paypal.php
+++ paypal.php
@@ -1,17 +1,17 @@
 <?php
 /*
 Plugin Name: Gravity Forms PayPal Standard Add-On
-Plugin URI: https://www.gravityforms.com
+Plugin URI: https://gravityforms.com
 Description: Integrates Gravity Forms with PayPal Payments Standard, enabling end users to purchase goods and services through Gravity Forms.
-Version: 3.2
-Author: rocketgenius
-Author URI: https://www.rocketgenius.com
+Version: 3.4
+Author: Gravity Forms
+Author URI: https://gravityforms.com
 License: GPL-2.0+
 Text Domain: gravityformspaypal
 Domain Path: /languages
 
 ------------------------------------------------------------------------
-Copyright 2009-2016 rocketgenius
+Copyright 2009-2020 Rocketgenius, Inc.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -30,7 +30,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 defined( 'ABSPATH' ) || die();
 
-define( 'GF_PAYPAL_VERSION', '3.2' );
+define( 'GF_PAYPAL_VERSION', '3.4' );
 
 add_action( 'gform_loaded', array( 'GF_PayPal_Bootstrap', 'load' ), 5 );
 
